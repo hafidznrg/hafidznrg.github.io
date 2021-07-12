22f67e40ca55b32096dcea0ee2cc7ff6e692f80b
hafidznrg <hafidznur.hn@gmail.com>
2021-07-12 07:34:10 +0700

    add about page and project page

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ $( '#navbar .navbar-nav div a' ).on( 'click', function () {
 		$( '#navbar .navbar-nav' ).find( 'div.active' ).removeClass( 'active' );
 		$( this ).parent( 'div' ).addClass( 'active' );
 	};
+	$( '#navbar .navbar-nav' ).find( 'div.active' ).removeClass( 'active' );
+	$( this ).parent( 'div' ).addClass( 'active' );
 });
 
 // $(window).scroll(function (event) {
@@ -17,7 +19,10 @@ $( '#navbar .navbar-nav div a' ).on( 'click', function () {
 // });
 
 $( '#navbar .navbar-toggler' ).on('click', function() {
-    $( '#navbar .navbar-nav' ).find( 'div.active' ).removeClass( 'active' );
+    // $( '#navbar .navbar-nav' ).find( 'div.active' ).removeClass( 'active' );
+	if ($('#navbar .navbar-toggler').attr('aria-expanded') === 'true') {
+		$( '#navbar .navbar-collapse .box-nav' ).addClass( 'nav-responsive' );
+	};
 });
 
 $("#wrapper").click( function() {
